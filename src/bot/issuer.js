5aaf874d1e30865335dc706e669c7c76280e88c1
Guilherme Lunhani <lunhanig@gmail.com>
2018-07-25 17:53:29 -0300

    enviando erro de login ou url de autenticação para usuario durante o login do id.org

--- src/bot/issuer.js
+++ src/bot/issuer.js
@@ -1,17 +1,34 @@
 const { Issuer } = require('openid-client');
+const logger = require('logger')
+const app = require('../app')
 
-module.exports = function(openid){
+module.exports = function(openid, msg){
     return Issuer.discover(openid.issuer).then(function (idIssuer) {
-	logger.debug('Attempting login for'+ msg.chat.id+' in '+openid.issuer)
+	return app.service('bot').create({
+	    id: msg.chat.id,
+	    message: {
+		type: 'string',
+		message: 'Encaminhado login...'
+	    }
+	})
+    }).then(function(){
 	return new idIssuer.Client({
 	    client_id: openid.clientID,
 	    client_secret: openid.clientSecret
 	})
     }).then(function(idOrg){
-	logger.debug(idOrg)
-	return id.authorizationUrl({
+	return idOrg.authorizationUrl({
 	    redirect_uri: openid.domain + openid.successRedirect,
 	    scope: 'openid email',
 	});
+    }).catch(function(err){
+	app.service('bot').create({
+		id: msg.chat.id,
+	    message: {
+		type: 'string',
+		message: 'Erro '+err.statusCode + ': '+err.message
+		}
+	})
+	logger.debug(err)
     })
 }
