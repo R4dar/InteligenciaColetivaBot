c27a3c37b8402a0381d2a70f86e654a8018daa17
hacker <hacker@dev.org.br>
2018-08-20 02:25:36 +0000

    limado suporte à websockets, por enquanto, para adequar o projeto às linhas de código relevantes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const express = require('@feathersjs/express');
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
-const channels = require('./channels');
 const mongoose = require('./mongoose');
 const authentication = require('./authentication');
 const swagger = require('feathers-swagger');
@@ -56,9 +55,6 @@ app.configure(authentication);
 
 // Set up our services (see `services/index.js`)
 app.configure(services);
-
-// Set up event channels (see channels.js)
-app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
