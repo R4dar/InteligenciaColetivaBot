bce3ee12e6541cc5a663039d2c0cf3d40a745c47
Guilherme Lunhani <lunhanig@gmail.com>
2018-09-26 13:52:06 -0300

    fixado loop em objetos que definem comandos do bot

--- src/services/bot/bot.manager.js
+++ src/services/bot/bot.manager.js
@@ -39,10 +39,12 @@ class TelegrafManager {
   build (app, telegraf) {
     Object.keys(this.manager).forEach(manages => {
       // console.log(`${this.manager[manages]}`);
-      const t = `${this.manager[manages]}`;
+      const t = this.manager;	 
+      console.log(manages);
+      console.log(t[manages]);
       // now manages every command defined by objects as templates
-      t.map(command => {
-
+      Object.keys(t[manages]).map(command => {
+        console.log(command);
         // A command is defined by slash with the command name
         telegraf.command(command, (ctx) => {
           app.service('users').find({ telegramId: ctx.chat.id }).then(function(res){
